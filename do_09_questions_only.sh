3298722585f2b65d3cfec1c25ddede8839c6c700
David Liebman <radiodee1@gmail.com>
2024-10-12 08:27:26 -0400

    new file

---
+++ do_09_questions_only.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+cd src 
+echo $PWD 
+
+if [ -f '../jsonl/llm.questions.txt' ]; then
+    echo 'train file exists'
+    exit 
+fi 
+
+touch ../jsonl/subject.txt
+
+
+while read x; do
+    Y='../../chatterbot-corpus/chatterbot_corpus/data/english/'${x}'.yml'
+    if [ -f $Y ]; then 
+        echo $y 
+    else 
+        echo 'subject file not found'
+        echo $Y
+        exit
+    fi
+
+done < '../jsonl/subject-questions.txt'
+
+while read x; do 
+    echo $x
+    ./train.py --questions $x
+done < '../jsonl/subject-questions.txt'
+
+echo '+++++++++++++++++'
+cat ../jsonl/llm.questions.txt
